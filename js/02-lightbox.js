6b4ad29ca2e9926ff85af1a27da8820569b059cf
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-06-04 12:31:20 +0300

    create 2d task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageListEl = document.querySelector('.gallery');
+
+
+const createGalleryEl = ({
+    preview,
+    description,
+    original,
+ }) =>
+ `<li class="gallery__item">
+ <a class="gallery__link" href="${original}">
+ <img 
+ class="gallery__image"
+ src="${preview}" 
+ data-source="${original}"
+ alt="${description}">
+ </a>
+ </li>`;
+ 
+console.log(createGalleryEl)
+
+const galleryCardsList = galleryItems.map((el) => createGalleryEl(el))
+    
+console.log(...galleryCardsList)
+imageListEl.insertAdjacentHTML("afterbegin", galleryCardsList.join(""))
